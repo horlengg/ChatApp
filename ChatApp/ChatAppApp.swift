bdc6a02a5ed2600ee0bfda429bb47d54e95d3c62
Leng <horleng123@gmail.com>
2026-06-07 10:05:02 +0700

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Houleng Ly on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
